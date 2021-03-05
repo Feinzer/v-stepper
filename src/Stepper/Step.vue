89c562da67cf95b0ea8bc2615af9b602a18b16a7
Daniel Hernández <danhernan23i@gmail.com>
2021-03-05 05:24:29 -0400

    Add step component

---
+++ src/Stepper/Step.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  name: 'Step',
+  computed: {
+    index() {
+      return this.$parent.$children.findIndex(child => child == this);
+    },
+    isCurrent() {
+      return this.index == this.$parent.currentIndex;
+    },
+  },
+};
+</script>
+
+<template>
+  <transition v-if="isCurrent" name="step">
+    <div class="step">
+      <slot />
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.step {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.step-enter-active,
+.step-leave-active {
+  transition-property: all;
+  transition-duration: 250ms;
+}
+
+.step-enter,
+.step-leave-active {
+  opacity: 0;
+}
+
+.step-enter {
+  transform: translateX(var(--stepper_forward));
+}
+.step-leave-active {
+  transform: translateX(var(--stepper_backward));
+}
+</style>
