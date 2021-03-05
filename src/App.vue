78b8e1145a66a56f030f582162f58a3374345cff
Daniel Hernández <danhernan23i@gmail.com>
2021-03-05 04:14:20 -0400

    Initial configuration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Stepper>
+      <Step><p>a</p></Step>
+      <Step><p>b</p></Step>
+      <Step><p>c</p></Step>
+      <Step><p>d</p></Step>
+      <Step><p>e</p></Step>
+      <Step><p>f</p></Step>
+    </Stepper>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Stepper from '@/Stepper/Stepper';
+import Step from '@/Stepper/Step';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    Stepper,
+    Step,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Ubuntu, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
